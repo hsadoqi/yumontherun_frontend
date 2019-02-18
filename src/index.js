4375a04b89c32401ec1f5a5fab203e1f008051f1
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-02-17 19:12:08 -0500

    incorporated geocoding & fetched trucks

--- src/index.js
+++ src/index.js
@@ -1,63 +1,103 @@
 const truckAPI = `https://data.sfgov.org/resource/6a9r-agq8.json`
-// checks for address in localStorage, gets address otherwise 
+let map
+let userLocation = {lat: 37.7946, lng: -122.3999}
+let locationFilter
 
-// automatically load map AND ask for location (add button to recheck address)
+// on load, checks localStorage for address
+// default position: downtown San Francisco 
+// asks for permission to geolocalize user 
 
 function init(){
     let address = localStorage.getItem('address')
     if(address){
-        let userAddress = JSON.parse(address)
-        loadMap(userAddress)
-    } else {    
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-         let userLocation = JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude})
-         localStorage.setItem('address', userLocation)
-         let userAddress = JSON.parse(userLocation)
-         loadMap(userAddress)
-        });
+        userLocation = JSON.parse(address)
+    } else {
+        geolocalizeUser()    
     }
+    loadMap(userLocation)
 }
 
-document.addEventListener('click', () => {
+// function invoked when user selects to geolocalize position
+
+function geolocalizeUser(){
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+         let userAddress = JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude})
+         localStorage.setItem('address', userAddress)
+         userLocation = JSON.parse(userAddress)
+         loadMap(userLocation)
+        });
+}
+
+// handles event listeners on app
+
+document.addEventListener('click', (e) => {
     if(e.target.id === 'submit-btn'){
-        submitAddress(e)
+        geocodeAddress(e)
+    } else if(e.target.id === 'find-btn'){
+        geolocalizeUser()
     }
 })
 
-// loads map based on user locatio
-function submitAddress(e){
-    console.log(e.target.previousElementSibling.value)
+// finds lat/long of user input & loads map
+
+function geocodeAddress(e){
+    let newAddress = e.target.previousElementSibling.value 
+    let geocoder = new google.maps.Geocoder()
+
+    geocoder.geocode({'address': newAddress}, function(results, status) {
+        if (status === 'OK') {
+            userLocation = results[0].geometry.location
+            map.setCenter(userLocation)
+            loadMap(userLocation)
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
 }
+
+// loads map
 
 function loadMap(userLocation){
-    let map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: userLocation});
-    new google.maps.Marker({position: userLocation, map: map});
-
+    map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: userLocation});
+    createMarker(userLocation)
+    fetchAllTrucks()
 }
 
-// submitBtn.addEventListener('submit', (e) => {
-//     console.log(e.target)
-// })
+// creates map marker
 
-// get address from input and apply to map 
+function createMarker(position){
+    new google.maps.Marker({map: map, position: position})
+}
 
+// fetch all trucks in api
 
-// fetch trucks in area based on map location 
-    // fetch(truckAPI)
-    // .then(res => res.json())
-    // .then(trucks => {
-    //     trucks.forEach(truck => {
-    //         if(truck.longitude !== "0" && truck.latitude !== "0"){
-    //             console.log(truck)
-    //         }
-    //     })
-    // })
+function fetchAllTrucks(){
+    fetch(truckAPI)
+    .then(res => res.json())
+    .then(trucks => {
+        trucks.forEach(truck => {
+            if(truck.longitude !== "0" && truck.latitude !== "0"){
+                filterTruck(truck)
+            }
+        })
+    })
+}
 
-// list trucks in area 
+// get distance between user & truck 
+// filter based on default filter/user input
 
+function filterTruck(truck){
+    let truckLocation = new google.maps.LatLng(truck.location.coordinates[1], truck.location.coordinates[0])
+    let newUserLocation = new google.maps.LatLng(userLocation["lat"], userLocation["lng"])
+    let distance = google.maps.geometry.spherical.computeDistanceBetween(truckLocation, newUserLocation);
+    console.log(distance)
+}
 
-// add markers to map of each truck's location 
+function showTruckResult(truck){
+    let truckList = document.getElementById('truck-list')
+}
+
 
 
 
