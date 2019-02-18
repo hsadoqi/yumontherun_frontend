288d27d1c3260248b8565a761fb5d3496172a43c
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-02-18 15:09:04 -0500

    finished core functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const truckAPI = `https://data.sfgov.org/resource/6a9r-agq8.json`
-let map
+const truckList = document.getElementById('truck-list')
+const truckResults = document.getElementById('results')
 let userLocation = {lat: 37.7946, lng: -122.3999}
+let truckInfo = document.getElementById('truck-info')
 let trucks = []
 let results = 20
+let markers = []
+let map
+console.log(truckList)
 
 // on load, checks localStorage for address
 // default position: downtown San Francisco 
 // asks for permission to geolocalize user 
 
 function init(){
-    let address = localStorage.getItem('address')
+    const address = localStorage.getItem('address')
     if(address){
         userLocation = JSON.parse(address)
     } else {
@@ -23,22 +28,39 @@ function init(){
 function geolocalizeUser(){
     navigator.geolocation.getCurrentPosition(
         function (position) {
-         let userAddress = JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude})
+         const userAddress = JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude})
          localStorage.setItem('address', userAddress)
          init()
         });
 }
 
-// handles event listeners on app
+// handles click events
 
 document.addEventListener('click', (e) => {
     e.preventDefault()
     if(e.target.id === 'find-btn'){
         geolocalizeUser()
     } else if(e.target.className === 'truck-item'){
-        let truckId = e.target.dataset.id
+        const truckId = e.target.dataset.id
         displayTruckInfo(truckId)
     } 
+})
+
+// handles submit events
+
+document.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if(e.target.id === 'map-input'){
+        geocodeAddress(e)
+    } 
+})
+
+// handles input events
+
+document.addEventListener('input', (e) => {
+    if(e.target.parentElement.id === 'search-food'){
+        searchFoodItems(e.target.value)
+    }
 })
 
 // finds lat/long of user input & loads map
@@ -62,11 +84,11 @@ function geocodeAddress(e){
 // loads map
 
 function loadMap(userLocation){
-    map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: userLocation});
-    createMarker(userLocation, {title: 'Your Location', id: null})
-
     trucks = []
     results = 20
+
+    map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: userLocation});
+    createMarker(userLocation, {title: 'Your Location', id: null, url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"})
 
     fetchAllTrucks()
 }
@@ -74,21 +96,27 @@ function loadMap(userLocation){
 // creates map marker
 
 function createMarker(position, options){
-    let marker = new google.maps.Marker({map: map, position: position, title: options['title'], id: options['id']})
-    let infowindow = new google.maps.InfoWindow({ content: options["title"]})
-
-    if(options['id'] === null){
-        infowindow.open(map, marker)
-    }
+    let marker = new google.maps.Marker({map: map, position: position, title: options['title'], id: options['id'], icon: { url: options['url']}})
 
     marker.addListener('click', () => {
         if(marker.id !== null){
             displayTruckInfo(marker.id)
-            infowindow.open(map, marker)
-            setTimeout(() => {infowindow.close()}, '2000');
-        }
+            clickMarker(marker)
+        } 
     })
+
+    markers.push(marker)
 }
+
+// click marker event handler 
+
+function clickMarker(marker){
+    let infowindow = new google.maps.InfoWindow({ content: marker["title"]})
+
+    infowindow.open(map, marker)
+    setTimeout(() => {infowindow.close()}, '2000');
+}
+
 
 // fetch all trucks in api
 
@@ -102,12 +130,11 @@ function fetchAllTrucks(){
             }
         })
     }).then(() => {
-        if(trucks.length !== 0){
+        if(trucks[0].distanceFromUser <= 24000){
             trucks.sort(compareTruckDistance)
             showTruckResults()
         } else {
-            let truckList = document.getElementById('truck-list')
-            truckList.innerHTML = ''
+            truckList.innerHTML = 'No results'
             alert('there are no trucks nearby, please choose a different address')
         }
     })
@@ -126,29 +153,34 @@ function getTruckDistance(truck){
 
 // sort trucks in object & render info for nearby trucks
 
-function showTruckResults(search = null){
-    let truckList = document.getElementById('truck-list')
+function showTruckResults(){
     truckList.innerHTML = ''
 
-    if(!search){
-        for(let i = 0; i <= results; i++){
-            truckList.innerHTML += showTruck(trucks[i])
-        }
-    } else {
-        for(let i = 0; truckList.childElementCount <= results && i < trucks.length; i ++){
-            if(trucks[i].fooditems.toLowerCase().includes(search.toLowerCase())){
-                truckList.innerHTML += showTruck(trucks[i])
-            }
-        }
+    for(let i = 0; i <= results; i++){
+        truckList.innerHTML += showTruck(trucks[i])
     }
-
-    let truckResults = document.getElementById('results')
 
     truckResults.addEventListener('wheel', (e) => {
         if(truckResults.scrollTop === truckResults.scrollHeight - truckResults.clientHeight){
             loadMoreResults()
         }
     })
+}
+
+function searchFoodItems(search){
+    truckList.innerHTML = ''
+
+    for(let i = 0; i < markers.length; i++){
+        if(markers[i].id !== null){
+            markers[i].setMap(null)
+        }
+    }
+
+    for(let i = 0; truckList.childElementCount <= results && i < trucks.length; i ++){
+        if(trucks[i].fooditems.toLowerCase().includes(search.toLowerCase())){
+            truckList.innerHTML += showTruck(trucks[i])
+        }
+    }
 }
 
 // load more results for nearby trucks 
@@ -173,7 +205,7 @@ function compareTruckDistance(truck1, truck2){
 // display each truck's info on the results div
 
 function showTruck(truck){
-    createMarker({lat: truck.location.coordinates[1], lng: truck.location.coordinates[0]}, {title: truck.applicant, id: truck.objectid})
+    createMarker({lat: truck.location.coordinates[1], lng: truck.location.coordinates[0]}, {title: truck.applicant, id: truck.objectid, url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"})
 
     return `<li data-id=${truck.objectid} class="truck-item">
     ${truck.applicant}<br><br>
@@ -187,29 +219,20 @@ function showTruck(truck){
 
 function displayTruckInfo(truckId){
     let foundTruck = trucks.find(truck => truck.objectid === truckId)
-    let truckInfo = document.getElementById('truck-info')
+    let foundMarker = markers.find(marker => marker.id === truckId)
+    
+    clickMarker(foundMarker)
 
     truckInfo.innerHTML = `<h3>${foundTruck.applicant}</h3>
     <p>${foundTruck.address}</p>
     <a href="https://www.google.com/maps/search/?api=1&query=${foundTruck.location.coordinates[1]},${foundTruck.location.coordinates[0]}">Get Directions</a>
     <p>${foundTruck.fooditems}</p>
     `
+
     window.scrollTo(0, document.body.scrollHeight)
 }
 
 
-document.addEventListener("submit", (e) => {
-    e.preventDefault()
-    if(e.target.id === 'map-input'){
-        geocodeAddress(e)
-    } 
-})
-
-document.addEventListener('input', (e) => {
-    if(e.target.parentElement.id === 'search-food'){
-        showTruckResults(e.target.value)
-    }
-})
 
 
 
